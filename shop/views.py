3dafafb0177bf824a413e6c7e3fe20106eb5771a
magwindow <antosha.petrakov@yandex.ru>
2025-10-01 23:50:17 +0300

    Update class Index, add method get_queryset. Added template engine in _categories.html

--- shop/views.py
+++ shop/views.py
@@ -7,5 +7,11 @@ from .models import Category, Product
 class Index(ListView):
     """Главная страница"""
     model = Product
+    context_object_name = 'categories'
     extra_context = {'title': 'Главная страница'}
     template_name = 'shop/index.html'
+
+    def get_queryset(self):
+        """Вывод родительской категории"""
+        categories = Category.objects.filter(parent=None)
+        return categories
