9757dce4aad34acd8f498d80dbb575dcbc269c67
magwindow <antosha.petrakov@yandex.ru>
2025-10-01 19:17:51 +0300

    Refactor register model Category in admin.py in shop

--- shop/admin.py
+++ shop/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from .models import Product, Category, Gallery
 
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'parent')
+    prepopulated_fields = {'slug': ('title',)}
+
+
 admin.site.register(Product)
-admin.site.register(Category)
 admin.site.register(Gallery)
